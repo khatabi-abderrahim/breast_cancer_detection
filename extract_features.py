d37264acbc7bbab62da3da0d271ce6704c8d8a92
Ale <alerodriguezgarcia@me.com>
2019-07-04 19:32:17 -0600

    Script to extract features from the image

--- extract_features.py
+++ extract_features.py
@@ -1,19 +1,29 @@
 import cv2
 import numpy as np
-from GLCM.pixel_matrix import number_of_pixels, create_matrix, pixel_relationship
+from GLCM.pixel_matrix import *
 
+"""
+Script to gextract the features from the image
+"""
+
+# image
 image = cv2.imread('all-mias/mdb001.pgm',0)
 image_shape = image.shape
 image_rows = image_shape[0]
 image_columns = image_shape[1]
 
-pixels = number_of_pixels(image, image_rows, image_columns)
+example_image = np.array([[0, 0, 1, 1],
+						  [0, 0, 1, 1],
+						  [0, 2, 2, 2],
+						  [2, 2, 3, 3],
+				])
+
+pixels = number_of_pixels(example_image)
 print(pixels)
 
-matrix = create_matrix(pixels)
-print(matrix)
+first_pixels = pixels[:4]
 
-print(matrix.shape)
+c_matrix = co_ocurrency_matrix_horizontal()
 
-pixel_rel = pixel_relationship(image, image_rows, image_columns, pixels[0], pixels[0])
-print("number of relationships (0,0) : {}".format(pixel_rel))
+new_matrix = c_matrix.horizontal_relationship(example_image, pixels)
+print (new_matrix)
