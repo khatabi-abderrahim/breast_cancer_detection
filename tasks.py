e8025a671fb6c300c9ffba971c0b842a7fa278ca
Ale <alerodriguezgarcia@me.com>
2019-09-09 07:02:25 -0600

    Add the image_number to use it to name the glcm result text file

--- tasks.py
+++ tasks.py
@@ -5,7 +5,7 @@ from GLCM.pixel_matrix import co_ocurrency_matrix_vertical
 app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 @app.task()
-def get_vertical_glcm_matrix(image_file_location, pixels_file_location):
+def get_vertical_glcm_matrix(image_number, image_file_location, pixels_file_location):
 	"""
 	This function is made to delegate the GLCM vertical relationship calculations
 	in an asynchronous way.
@@ -14,6 +14,6 @@ def get_vertical_glcm_matrix(image_file_location, pixels_file_location):
 		image_file_location
 	"""
 	
-	co_ocurrency_matrix_vertical().vertical_relationship_probabilities(image_file_location, pixels_file_location)
+	co_ocurrency_matrix_vertical().vertical_relationship_probabilities(image_number, image_file_location, pixels_file_location)
 
 	return None
