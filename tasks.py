3713a8e00bf27835618b93f442f552a01a5ae110
Ale <alerodriguezgarcia@me.com>
2019-09-15 20:11:12 -0600

    This function is made to delegate the calculationd of the texture features of the GLCM in an asynchronous way.

--- tasks.py
+++ tasks.py
@@ -1,20 +1,23 @@
 from celery import Celery
 import numpy
-from GLCM.pixel_matrix import CoOcurrencyMatrixVertical, co_ocurrency_matrix_horizontal
+from GLCM.textures_measurements import TextureMeasurements
 from helper_functions import write_text_files
 
 app = Celery('tasks', backend='amqp', broker='amqp://')
 
 @app.task()
-def get_vertical_glcm_matrix(image_number, image_file_location, pixels_file_location):
+def get_texture_glcm_measurements(image_number, image_file_location):
 	"""
-	This function is made to delegate the GLCM vertical relationship calculations
-	in an asynchronous way.
+	This function is made to delegate the calculationd of the texture features
+	of the GLCM in an asynchronous way.
 
 	Arguments:
-		image_file_location
+		image_file_location (string): The location of the image
+		image_number (number): A number that identifies the image to be analyzed
+
+	Returns:
+		result (string): Returns the 
 	"""	
-	
-	result = CoOcurrencyMatrixVertical().vertical_relationship_probabilities(image_number, image_file_location, pixels_file_location)
+	result = TextureMeasurements().contrast_measurements(image_number, image_file_location)
 
 	return result
