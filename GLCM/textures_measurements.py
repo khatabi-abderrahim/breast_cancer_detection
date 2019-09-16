5e2c534d14a07981f2dfc99ad8a067f80634b534
Ale <alerodriguezgarcia@me.com>
2019-09-15 20:01:32 -0600

    Get the contrast measurements of the image from the glcm percentage matrix using the scikit library and store the result in a text file

--- GLCM/textures_measurements.py
+++ GLCM/textures_measurements.py
@@ -1,41 +1,39 @@
+from skimage import feature
+import numpy
 import math
+from GLCM.pixel_matrix import CoOcurrencyMatrix
 
-"""
-They are Energy, Entropy, Contrast, Correlation, and  Homogeneity. Energy
-returns the sum  of  squared elements in the GLCM and the range will be in
-[0, 1]. Entropy measures the randomness  of  intensity  distribution.
-Correlation measure of  image  linearity, and Homogeneity Returns a value that
-measures the closeness of the distribution of elements in the GLCM to the GLCM
-diagonal and range will be in [0 1].
-"""
+class TextureMeasurements():
+	"""
+	They are Energy, Entropy, Contrast, Correlation, and  Homogeneity. Energy
+	returns the sum  of  squared elements in the GLCM and the range will be in
+	[0, 1]. Entropy measures the randomness  of  intensity  distribution.
+	Correlation measure of  image  linearity, and Homogeneity Returns a value that
+	measures the closeness of the distribution of elements in the GLCM to the GLCM
+	diagonal and range will be in [0 1].
+	"""
 
-class texture_measurements():
-	def contrast_group_measurements(self, glcm_percentage_matrix, pixel_set):
+	def contrast_measurements(self, image_number, image_file_location):
 		"""
 		Get the contrast measurements of the image from the glcm percentage matrix.
 		Contrast measures the  amount  of local  variation  in  the  image.
+		feature.greycoprops(prop='contrast'): The tecture property to be 
+											  calculated from the GLCM matrix
 		
 		Args:
 			glcm_percentage_matrix (array): The percentage of the relationship between
 											two pixels in the image
-			pixel_set (array): An array with the set of pixels in the image
+			image_number (number): A number that identifies the image to be analyzed
 
 		Returns:
-			contrast (number): The contrast probability level of the image
-			dissimilarity (number): The dissimilarity probability level of the image
-			homogeneity (number): The homogeneity probability level of the image
+			(string): The location of where the contrast information 
+					  of the matrix will be saved
 		"""
-		contrast = 0
-		dissimilarity = 0
-		homogeneity = 0
-		
-		for reference in pixel_set:
-			for neighbour in pixel_set:
-				contrast += glcm_percentage_matrix[reference][neighbour] * math.pow((pixel_set[reference]-pixel_set[neighbour]),2)
-				dissimilarity += glcm_percentage_matrix[reference][neighbour] * abs(pixel_set[reference]-pixel_set[neighbour])
-				homogeneity += float(glcm_percentage_matrix[reference][neighbour]) / float(1 + math.pow((pixel_set[reference]-pixel_set[neighbour]),2))
+		glcm_percentage_matrix = CoOcurrencyMatrix().relationship_probabilities(image_file_location)
+		contrast = feature.greycoprops(glcm_percentage_matrix, prop='contrast')
+		numpy.savetxt("GLCM/matrix/contrast_mdb{}.txt".format(image_number), contrast)
 
-		return contrast, dissimilarity, homogeneity
+		return "GLCM/matrix/contrast_mdb{}.txt".format(image_number)
 
 	def energy_measurements(self, glcm_percentage_matrix, pixel_set):
 		"""
