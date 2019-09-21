0f440d4fee8e1aa5a0c415f2a0d998c3b3229753
Ale <alerodriguezgarcia@me.com>
2019-09-20 22:33:39 -0600

    Convert a string type element array into a interger type element array.

--- GLCM/GLCM_helper_functions.py
+++ GLCM/GLCM_helper_functions.py
@@ -31,19 +31,19 @@ def string_array_to_int_array(string_array):
 
 	Returns:
 		string_array (array): An integer element array.
-	"""
-
+	"""	
 	for array_element in range(0,len(string_array)):
+		string_array[array_element] = string_array[array_element].replace("\n","")
 		string_array[array_element] = string_array[array_element].replace("[","")
 		string_array[array_element] = string_array[array_element].replace("]"," ")
 		string_array[array_element] = string_array[array_element].replace("  ",",")
 		string_array[array_element] = string_array[array_element].replace(" ",",")
 		string_array[array_element] = string_array[array_element].split(',')
-		
+
 		for index, element in enumerate(string_array[array_element]):
 			if len(element) == 0:
 				del string_array[array_element][index]
 
-		string_array[array_element] = [float(string_element) for string_element in string_array[array_element]]
+		string_array[array_element] = [float(element) for element in string_array[array_element]]
 
 	return string_array
