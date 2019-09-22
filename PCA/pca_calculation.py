c3900dbce028fe81e99c8570a423e99f66b083dc
Ale <alerodriguezgarcia@me.com>
2019-09-22 17:01:39 -0600

    Deletes unecesary imports and functions as well as correct naming of a class

--- PCA/pca_calculation.py
+++ PCA/pca_calculation.py
@@ -1,29 +1,6 @@
 import numpy
-import pca_math
 
-class  pca_matrix_creation():
-	"""
-	Create the PCA matrix with the GLCM variables of every image in the dataset.
-	"""
-	def read_text_file(self):
-		"""
-		Extracts the GLCM measurments from the file and creates an array with the data
-
-		Returns:
-			glcm_measurements (array): The GLCM measurements data
-		"""
-		text_file_lines = open("Results/GLCM_results.txt","r").readlines()
-
-		glcm_measurements = []
-
-		for element in text_file_lines:
-			element = element.split('\n')
-			element = float(element[0])
-			glcm_measurements.append(element)
-
-		return glcm_measurements
-
-class pca_calculation():
+class PCACalculation():
 	"""
 	Calculate the principal components from the GLCM measurements to validate wich
 	one of them will be able to predict if the image has a malformation
