be92af755ed29883063dd3f7a47c440512978037
Ale <alerodriguezgarcia@me.com>
2019-09-25 22:57:55 -0600

    Corrections in transforming the string elements from the given list to a numpy integer matrix

--- GLCM/GLCM_helper_functions.py
+++ GLCM/GLCM_helper_functions.py
@@ -12,18 +12,18 @@ def string_array_to_int_array(string_array):
 	Returns:
 		string_array (array): An integer element array.
 	"""	
-	for array_element in range(0,len(string_array)):
-		string_array[array_element] = string_array[array_element].replace("\n","")
-		string_array[array_element] = string_array[array_element].replace("[","")
-		string_array[array_element] = string_array[array_element].replace("]"," ")
-		string_array[array_element] = string_array[array_element].replace("  ",",")
-		string_array[array_element] = string_array[array_element].replace(" ",",")
-		string_array[array_element] = string_array[array_element].split(',')
+	integer_array = []
+	for list_element in range(0, len(string_array)):
+		string_array[list_element] = string_array[list_element].replace("\n","")
+		string_array[list_element] = string_array[list_element].replace("[","")
+		string_array[list_element] = string_array[list_element].replace("]"," ")
+		string_array[list_element] = string_array[list_element].replace("  ",",")
+		string_array[list_element] = string_array[list_element].replace(" ",",")
+		string_array[list_element] = string_array[list_element].split(',')
 
-		for index, element in enumerate(string_array[array_element]):
-			if len(element) == 0:
-				del string_array[array_element][index]
+	for list_element in range(0, len(string_array)):
+		string_array[list_element] = list(filter(None, string_array[list_element]))
+		string_array[list_element] = [float(element) for element in string_array[list_element]]
+		integer_array = numpy.append(integer_array,string_array[list_element])
 
-		string_array[array_element] = [float(element) for element in string_array[array_element]]
-
-	return string_array
+	return integer_array
