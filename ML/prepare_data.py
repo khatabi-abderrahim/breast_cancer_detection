93f4c790d4be2d9aba9bbd9b450b4a0a8302480c
Ale <alerodriguezgarcia@me.com>
2019-10-07 20:02:56 -0600

    Splits all the result data and the variable data into two sets: training (80 % of the data) and testing (20 % of the data).

--- ML/prepare_data.py
+++ ML/prepare_data.py
@@ -29,3 +29,28 @@ class PrepareData():
 
 		return texture_matrix
 
+	def create_training_and_test_data_sets(self, label_matrix):
+		"""
+		Splits all the result data and the variable data into two sets: training (80 %
+		of the data) and testing (20 % of the data).
+
+		Args:
+			label_matrix (matrix): The labels results gotten from the all-mias dataset.
+
+		Returns:
+			data_train (matrix): The data that will be used to train the algorithm.
+			data_test (matrix): The data that will be used to test the algorithm.
+			label_train (matrix): The result data that will be used to test the algorithm.
+			label_test (matrix): The result data that will be compared to the predicted data
+								 to get how correct is the algorithm.
+		"""
+		principal_components = ['contrast right', 'contrast left', 'contrast up',
+		'contrast down', 'energy right', 'energy left', 'energy up', 'energy down',
+		'dissimilarity right', 'dissimilarity left', 'dissimilarity up',
+		'dissimilarity down', 'homogeneity right', 'homogeneity left', 'homogeneity up',
+		'homogeneity down', 'correlation right', 'correlation left']
+
+
+		data_train, data_test, label_train, label_test = train_test_split(self.prepare_texture_matrix(principal_components=principal_components), label_matrix, test_size=0.20)
+
+		return data_train, data_test, label_train, label_test
