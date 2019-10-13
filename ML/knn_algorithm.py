8c7a716c71c37b30a140adc797d6f70090b2c29e
Ale <alerodriguezgarcia@me.com>
2019-10-13 16:25:49 -0600

    Get the neighbour with that gets the smallest error

--- ML/knn_algorithm.py
+++ ML/knn_algorithm.py
@@ -14,7 +14,7 @@ class KnnAlgorithm():
 				training_and_test_data[3]: label_test
 
 		Returns:
-			errosrs (dictionary): An array with that maps all the obtained erros with the
+			errors (dictionary): An array with that maps all the obtained erros with the
 								  number of neighbours.
 		"""
 		errors = {}
@@ -28,3 +28,23 @@ class KnnAlgorithm():
 		    errors[neighbors] = numpy.mean(predict_labels != training_and_test_data[3])
 
 		return errors
+
+	def calculate_minimum_error(self, training_and_test_data):
+		"""
+		Get the neighbour with that gets the smallest error
+		
+		Args:
+			training_and_test_data (Tuple)
+
+		Returns:
+			best_neighbor (number): the neighbour with that gets the smallest error
+		"""
+		errors = self.calculate_neighbors_errors(training_and_test_data)
+		smallest_error = 1
+		best_neighbor = 0
+
+		for errors_key, errors_value in errors.items():
+			if errors_value < smallest_error :
+				best_neighbor = errors_key
+
+		return best_neighbor
