daf0a2036ecd004f0e4dac7b4a1eaf30907e0225
Ale <alerodriguezgarcia@me.com>
2019-10-13 22:59:59 -0600

    Constructor method that initialices the result labels used to train the algorithm and the actual results.

--- ML/algorithm_metrics.py
+++ ML/algorithm_metrics.py
@@ -1,12 +1,24 @@
 import numpy
 from sklearn.metrics import multilabel_confusion_matrix
+from PCA.matrix_creation import MatrixCreation
+from ML.prepare_data import PrepareData
 
 class ConfusionMatrix():
 	"""
 	It's a specific table layout that allows visualization of the performance
 	of an algorithm
 	"""
-	def create_confusion_matrix(self, true_results, predicted_results):
+	def __init__(self, predicted_results):
+		"""
+		Constructor method that initialices the result labels used to train the
+		algorithm and the actual results.
+		"""
+		label_matrix = MatrixCreation().create_full_matrix()[:,range(24,36)]
+
+		self.label_test = PrepareData().create_training_and_test_data_sets(label_matrix=label_matrix)[3]
+		self.predicted_results = predicted_results
+
+	def create_confusion_matrix(self):
 		"""
 		Creates the Confusion Matrix for each of the labels.
 		
@@ -22,6 +34,6 @@ class ConfusionMatrix():
 			normalized_confusion_matrix (matrix): A Multidimensional array conaining the
 											      normalized confusion matrix of every label. 
 		"""
-		labels_confusion_matrixes = multilabel_confusion_matrix(true_results, predicted_results)
+		labels_confusion_matrixes = multilabel_confusion_matrix(self.label_test, self.predicted_results)
 
 		return labels_confusion_matrixes
